83896595f0e82aa7d7618d851a7297ea69990987
fellowsyamabe <fellows.yamabe@gmail.com>
2026-03-01 13:19:23 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // 👇 GitHub Pages用（超重要）
-  base: "/CocoLablp/",   // ← ここにカンマ追加
+  base: "/Liveragencylp/"
 
   plugins: [
     react(),
